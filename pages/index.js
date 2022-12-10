a0a13dce3d07087ec3a6856f6a63864f0c627208
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-12-10 14:21:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -164,7 +164,7 @@ export default function Home({ posts }) {
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="/public/static/Dharmik_Resume.pdf">
+                  <Link href="https://drive.google.com/file/d/1_0hejMB01T0tMteO6OcTM3qbcaggHEiB/view?usp=share_link">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <svg
