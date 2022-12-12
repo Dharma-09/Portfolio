c575b213f29bc87dd469b5db017ffc2ff0e6f0b8
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-12-12 18:02:42 +0530

    Update experienceData.js

--- data/experienceData.js
+++ data/experienceData.js
@@ -1,29 +1,16 @@
 const experienceData = [
   {
-    title: 'Data Engineering Intern',
-    company: 'Accenture',
-    location: 'Ahmedabad, India',
-    range: 'January 2022 - May 2022',
-    url: 'https://www.accenture.com/gr-en/about/consulting-index',
+    title: 'Smart Contract Auditor(Individual)',
+    company: 'Code4rena',
+    location:'',
+    range: 'March 2021 - Present',
+    url: 'https://code4rena.com/leaderboard',
     text1:
-      'Helped with the digital transformation of BOAT by working on their platform called ByteETL.',
+      'I am certified warden at code4rena',
     text2:
-      'Built the encryption component for the platform using Scala and Spark which included 2 encoding functionalities : ENCRYPT and HASH.',
+      'I have found Medium severity vulnerabilities along with lots of gas optimization and code optimization report',
     text3:
-      'Created JSON wrappers for the encryption components and automated and triggered the pipelines to run at specific intervals using CronJob.',
-  },
-  {
-    title: 'Software Engineer Analytics',
-    company: 'HOPS Healthcare',
-    location: 'Ahmedabad, India',
-    range: 'March 2021 - June 2021',
-    url: 'https://hops.healthcare/',
-    text1:
-      'Guided by Mr. Vivek Patel, to create a pipeline that extracts essential information from informal conversations.',
-    text2:
-      'Worked on creating a web app for their healthcare system for doctors to analyze and save reports.',
-    text3:
-      'Developed functions for automatically extracting important elements from reports using BioBERT and Regex.',
+      'Developed script for automatically extracting gas optimization issue from given scope using Golang and Regex.',
   },
 ]
 
