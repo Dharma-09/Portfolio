5b7aa55cf04e1731600443e83e671d1738a3529f
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2023-05-11 10:56:08 +0530

    Update experienceData.js

--- data/experienceData.js
+++ data/experienceData.js
@@ -1,4 +1,16 @@
 const experienceData = [
+    { title: 'Secureum RACE result',
+    company: 'Secureum',
+    location:'',
+    range: '',
+    url: 'https://discord.com/channels/814328279468474419/953888880967438398',
+    text1:
+      'RACE#15 : I Ranked 28 outof 186 participate with 5/8 score.',
+    text2:
+      'RACE#16 : I Ranked 31 outof 205 participate with 4.2/8 score.',
+    text3:
+      'RACE#17 : I Ranked 36 outof 230 participate with 3.8/8 score.',
+  },
   {
     title: 'Smart Contract Auditor(Individual)',
     company: 'Code4rena',
